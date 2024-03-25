7e7e59ef65cc6538fa63c47467285a7a380a44ae
KaranMohanS <130283316+KaranMohanS@users.noreply.github.com>
2024-03-25 12:31:02 +0530

    Reverse Hellow Fully Pyramid Patterns

---
+++ Pattern Programs/ReverseHellowFullyPyramid.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+   int n;
+   printf("enter number");
+   scanf("%d",&n);
+
+   for(int i=n-1;i>=0;i--)
+   {
+    for(int j=0;j<2*(n-i)-1;j++)
+    {
+        printf(" ");
+    }
+    for(int k=0;k<2*i+1;k++)
+    {
+        if(k==0 || k==2*i || i==n-1)
+        printf("* ");
+        else
+        printf("  ");
+    }
+    printf("\n");
+   }
+}
