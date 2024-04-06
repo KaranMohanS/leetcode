e65c0e77b0c1bbd10cb1e6d5fec0f264ca96d083
KaranMohanS <130283316+KaranMohanS@users.noreply.github.com>
2024-04-06 12:38:53 +0530

    Replace 0 To 5 Given Array

---
+++ ARRAYproblems/ReplaceInteger.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+   int n;
+   printf("enter size");
+   scanf("%d",&n);
+
+   int arr[n];
+   for(int i=0;i<n;i++)
+   {
+    scanf("%d",&arr[i]);
+   }
+   
+   int m=5;
+   for(int i=0;i<n;i++)
+   {
+    if(arr[i]==0)
+    {
+        arr[i]=m;
+    }
+   }
+    for(int i=0;i<n;i++)
+   {
+    printf("%d",arr[i]);
+   }
+    
+}
